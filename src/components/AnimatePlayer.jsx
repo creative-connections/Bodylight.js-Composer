415c80f723f50b4cc8725bad8aac1d2044b4ee7f
David Polák <git@davidpolak.cz>
2018-07-17 14:40:03 +0200

    Animate player refactor

--- src/components/AnimatePlayer.jsx
+++ src/components/AnimatePlayer.jsx
@@ -8,26 +8,67 @@ class AnimatePlayer extends Component {
   constructor (props) {
     super(props)
     this.handleResize = this.handleResize.bind(this)
-
     this.canvas = React.createRef()
-    this.runtime = new Runtime(this.props.source, this.props.name)
+    this.runtime = null
+    this.createRuntime(this.props.source, this.props.name)
+  }
+
+  isRuntime () {
+    return this.runtime !== null
+  }
+
+  createRuntime (source, name) {
+    if (source !== null && name !== null) {
+      this.runtime = new Runtime(source, name)
+    }
+  }
+
+  initRuntime () {
+    if (this.isRuntime()) {
+      if (this.canvas.current !== null) {
+        this.runtime.init(this.canvas.current, true).then()
+      }
+    }
+  }
+
+  destroyRuntime () {
+    if (this.isRuntime()) {
+      this.runtime.destroy()
+      this.runtime = null
+    }
   }
 
   componentDidMount () {
-    createjs.Ticker.setFPS(60)
-    this.runtime.init(this.canvas.current, 'beaker', true).then(() => {
-    })
+    this.initRuntime()
   }
 
   componentWillUnmount () {
-    this.runtime.destroy()
+    this.destroyRuntime()
+  }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    if (nextProps.source !== this.props.source || nextProps.name !== this.props.name) {
+      this.destroyRuntime()
+      this.createRuntime(nextProps.source, nextProps.name)
+      return true
+    }
+    return false
+  }
+
+  componentDidUpdate () {
+    this.initRuntime()
   }
 
   handleResize (evt, data) {
-    this.runtime.resize(data.size.width, data.size.height)
+    if (this.isRuntime()) {
+      this.runtime.resize(data.size.width, data.size.height)
+    }
   }
 
   render () {
+    if (!this.isRuntime()) {
+      return null
+    }
     const width = this.props.width
     const height = this.props.height
 
