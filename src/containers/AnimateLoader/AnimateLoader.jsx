099599658d54209cee431b9c35a1d9f1137c7387
David Polák <git@davidpolak.cz>
2018-07-17 15:16:22 +0200

    AnimateInfo reference removed

--- src/containers/AnimateLoader/AnimateLoader.jsx
+++ src/containers/AnimateLoader/AnimateLoader.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 
 import AnimatePlayer from '@components/AnimatePlayer'
-import AnimateInfo from './AnimateInfo'
 import Runtime from '@helpers/Animate/Runtime'
 import NegativeOrPositiveButton from '@components/NegativeOrPositiveButton'
 import { Header, Grid, Divider, Transition, Segment } from 'semantic-ui-react'
