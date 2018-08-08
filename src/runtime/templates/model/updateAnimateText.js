92319a6104706babafe1ba2c8af7fea117454b8a
David Polak <git@davidpolak.cz>
2018-08-08 17:08:26 +0200

    AnimateText now changes with visible attribute

--- src/runtime/templates/model/updateAnimateText.js
+++ src/runtime/templates/model/updateAnimateText.js
@@ -1,6 +1,7 @@
 export default function updateAnimateText () {
   this.config.widgets.AnimateText.forEach(animateText => {
-    let value = animateText.transform(animateText.valueProvider())
-    animateText.widget.text = value
+    let value = animateText.valueProvider()
+    animateText.widget.text = animateText.transform(value)
+    animateText.widget.visible = animateText.visible(value)
   })
 }
