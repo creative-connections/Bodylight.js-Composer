523c063d3e752d343de11b1aaa09101748eb3b81
David Polak <git@davidpolak.cz>
2018-08-09 17:41:12 +0200

    Rudimentary error handling for bindWidgets

--- src/runtime/templates/model/bindWidgets.js
+++ src/runtime/templates/model/bindWidgets.js
@@ -1,12 +1,28 @@
 export default function bindWidgets () {
+  const bindWidget = (key, value) => {
+    try {
+      value.widget = value.widget.bind(this)()
+      return true
+    } catch (e) {
+      // FIXME: this should provide a concrete error in lookupWidget
+      console.warn(`Could not find widget for ${value}! Message: ${e.message}`)
+      return false
+    }
+  }
+
   Object.entries(this.config.widgets.controlledAnimateAnim).forEach(
     ([key, value]) => {
-      value.widget = value.widget.bind(this)()
+      if (!bindWidget(key, value)) {
+        delete this.config.widgets.controlledAnimateAnim[key]
+      }
     }
   )
+
   Object.entries(this.config.widgets.AnimateText).forEach(
     ([key, value]) => {
-      value.widget = value.widget.bind(this)()
+      if (!bindWidget(key, value)) {
+        delete this.config.widgets.AnimateText[key]
+      }
     }
   )
 }
