f95cb2fe7c9e58eb29421c22d203701ecbd2256b
David Polak <git@davidpolak.cz>
2018-08-14 18:38:46 +0200

    Runtime config builder for Range

---
+++ src/runtime/builders/widgets/Range/config.js
@@ -0,0 +1,20 @@
+import configureStore from '@src/configureStore'
+import { getConfigForRanges } from '@reducers'
+
+import update from 'immutability-helper'
+
+export default () => {
+  const ranges = getConfigForRanges(configureStore().store.getState())
+
+  const config = []
+  Object.entries(ranges).forEach(([name, configuration]) => {
+    const transform = Function(`return ${configuration.transform}`)
+
+    configuration = update(configuration, {
+      transform: {$set: transform}
+    })
+
+    config[name] = configuration
+  })
+  return config
+}
