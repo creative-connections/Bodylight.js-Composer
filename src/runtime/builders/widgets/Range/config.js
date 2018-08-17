7f6177a9e5591826b96c6de6ad02790b769dfee4
David Polak <git@davidpolak.cz>
2018-08-17 18:36:47 +0200

    Change Range config to Functionalized config

--- src/runtime/builders/widgets/Range/config.js
+++ src/runtime/builders/widgets/Range/config.js
@@ -3,17 +3,34 @@ import { getConfigForRanges } from '@reducers'
 
 import update from 'immutability-helper'
 
+const functionalize = (configuration, attribute) => {
+  let config = configuration[attribute]
+  if (config.function !== null) {
+    const fn = Function(`return ${config.function}`)()
+    config = update(config, {
+      function: {$set: fn}
+    })
+    configuration = update(configuration, {
+      [attribute]: {$set: config}
+    })
+  }
+  return configuration
+}
+
+const functionalizeAttributes = configuration => {
+  configuration.attributes.forEach(attribute => {
+    configuration = functionalize(configuration, attribute)
+  })
+  return configuration
+}
+
 export default () => {
   const ranges = getConfigForRanges(configureStore().store.getState())
 
   const config = {}
   Object.entries(ranges).forEach(([name, configuration]) => {
-    const transform = Function(`return ${configuration.transform}`)()
-
-    configuration = update(configuration, {
-      transform: {$set: transform}
-    })
-
+    configuration = functionalizeAttributes(configuration)
+    configuration = functionalize(configuration, 'target')
     config[name] = configuration
   })
   return config
