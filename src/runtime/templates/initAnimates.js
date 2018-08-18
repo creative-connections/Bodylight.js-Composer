ed950b415cf25d7ab029b77d6feef9287ae2c80b
David Polak <git@davidpolak.cz>
2018-08-18 15:26:36 +0200

    Fix initAnimates typo

--- src/runtime/templates/initAnimates.js
+++ src/runtime/templates/initAnimates.js
@@ -4,7 +4,7 @@ export default function initAnimates () {
   const promises = []
   Object.entries(animates).forEach(([name, source]) => {
     // find our canvas by name
-    const element = document.getElementsByName(name)[0]
+    const element = document.getElementsByName(`animate-${name}`)[0]
     if (typeof element === 'undefined') {
       return
     }
