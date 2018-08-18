6cde707abd1d73a644e1d09342e15a4fe6acebfc
David Polak <git@davidpolak.cz>
2018-08-19 00:31:50 +0200

    Add compact prop to GridRow

--- src/scenes/Connect/components/GridRow.jsx
+++ src/scenes/Connect/components/GridRow.jsx
@@ -9,20 +9,23 @@ const renderLabel = label => {
   return null
 }
 
-const GridRow = ({label, children}) => (
-  <Grid.Row style={{boxShadow: 'none'}}>
-    <Grid.Column width={2}>
+const GridRow = ({label, children, compact = false}) => {
+  let style = {}
+  if (compact === true) {
+    style = { padding: '0em 1em 0em 1em' }
+  }
+  return <Grid.Row style={{boxShadow: 'none'}}>
+    <Grid.Column width={2} style={style}>
       <Container textAlign='right'>
         {renderLabel(label)}
       </Container>
     </Grid.Column>
-    <Grid.Column width={14}>
+    <Grid.Column width={14} style={style}>
       <Container textAlign='left'>
         {children}
       </Container>
     </Grid.Column>
   </Grid.Row>
-
-)
+}
 
 export default GridRow
