3c4094c374fb40834c9d7acc7f13739412e7a574
David Polak <git@davidpolak.cz>
2018-08-27 15:19:11 +0200

    Fix undefined arguments in Action

--- src/runtime/builders/widgets/processAction.js
+++ src/runtime/builders/widgets/processAction.js
@@ -6,25 +6,29 @@ export default (action) => {
   for (let i = 0; i < action.args.length; i++) {
     const arg = action.args[i]
 
-    switch (arg.type) {
-      case ArgumentType.MODEL:
-        args += `getModelByID('${action.args[i].value}')`
-        break
-      case ArgumentType.ANIMATE_ANIM:
-        args += `getAnimateAnimByID('${action.args[i].value}')`
-        break
-      case ArgumentType.ANIMATE_TEXT:
-        args += `getAnimateTextByID('${action.args[i].value}')`
-        break
-      case ArgumentType.BUTTON:
-        args += `getButtonByID('${action.args[i].value}')`
-        break
-      case ArgumentType.RANGE:
-        args += `getRangeByID('${action.args[i].value}')`
-        break
-      case ArgumentType.TOGGLE:
-        args += `getToggleByID('${action.args[i].value}')`
-        break
+    if (arg === undefined || arg === null) {
+      args += `null`
+    } else {
+      switch (arg.type) {
+        case ArgumentType.MODEL:
+          args += `getModelByID('${action.args[i].value}')`
+          break
+        case ArgumentType.ANIMATE_ANIM:
+          args += `getAnimateAnimByID('${action.args[i].value}')`
+          break
+        case ArgumentType.ANIMATE_TEXT:
+          args += `getAnimateTextByID('${action.args[i].value}')`
+          break
+        case ArgumentType.BUTTON:
+          args += `getButtonByID('${action.args[i].value}')`
+          break
+        case ArgumentType.RANGE:
+          args += `getRangeByID('${action.args[i].value}')`
+          break
+        case ArgumentType.TOGGLE:
+          args += `getToggleByID('${action.args[i].value}')`
+          break
+      }
     }
 
     if (i + 1 !== action.args.length) {
