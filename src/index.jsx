cda44ac5618a2e70eaff3fd7935c2e797408170b
David Polak <git@davidpolak.cz>
2018-09-05 15:56:23 +0200

    Remove dead code

--- src/index.jsx
+++ src/index.jsx
@@ -34,10 +34,6 @@ class App extends Component {
 
 const {store, persistor} = configureStore()
 
-console.log(process.env)
-console.log(process.env.NODE_ENV)
-console.log(process.env.PATH)
-
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
