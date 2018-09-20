5dca374791d17f430152ec20926f5f8eb856a858
David Polak <git@davidpolak.cz>
2018-09-20 19:23:53 +0200

    Add quick&dirty export

--- src/scenes/Menu/components/MenuHeader.jsx
+++ src/scenes/Menu/components/MenuHeader.jsx
@@ -3,6 +3,9 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { Menu, Dropdown } from 'semantic-ui-react'
 import { newProject } from '@actions/actions'
+import { saveAs } from 'file-saver'
+
+import Builder from '@runtime'
 
 class MenuHeader extends Component {
   constructor (props) {
@@ -15,12 +18,19 @@ class MenuHeader extends Component {
     this.props.newProject()
   }
 
+  handleQuickExport () {
+    const builder = new Builder()
+    const html = new Blob([builder.build()], { type: 'text/html;charset=utf-8' })
+    saveAs(html, 'export.html')
+  }
+
   render () {
     return (
       <Fragment>
         <Dropdown item text='File'>
           <Dropdown.Menu style={{ minWidth: 15 + 'em' }}>
             <Dropdown.Item onClick={this.handleNew}>New</Dropdown.Item>
+            <Dropdown.Item onClick={this.handleQuickExport}>Quick export</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
       </Fragment>
