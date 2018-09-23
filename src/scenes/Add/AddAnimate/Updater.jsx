9389860e85edb0d36dc0d171dfe8ee08a4bab606
David Polak <git@davidpolak.cz>
2018-09-23 21:55:32 +0200

    Update AddAnimate to support updating animates WIP

---
+++ src/scenes/Add/AddAnimate/Updater.jsx
@@ -0,0 +1,93 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { toast } from 'react-toastify'
+import { Redirect } from 'react-router-dom'
+import { Grid, Form, Checkbox, Divider, Button } from 'semantic-ui-react'
+
+import { addAnimate } from '@actions'
+import { configGetAllAnimates } from '@reducers'
+import generateID from '@helpers/generateID'
+
+class AddAnimate extends Component {
+  constructor (props) {
+    super(props)
+    this.handleAdd = this.handleAdd.bind(this)
+    this.handleSelect = this.handleSelect.bind(this)
+
+    // check if we already have an animate with the same rootComponent
+    const animates = []
+    Object.entries(this.props.animates).forEach(([key, animate]) => {
+      if (animate.originalName === this.props.upload.root) {
+        animates.push(animate)
+      }
+    })
+
+    // if not, add
+    if (animates.length === 0) {
+      return this.handleAdd()
+    }
+
+    this.state = { animates }
+  }
+
+  handleAdd () {
+    const upload = this.props.upload
+    this.props.addAnimate(upload.js, upload.hash, upload.root, upload.components)
+    this.props.onUpdate(true)
+  }
+
+  handleSelect (e, {value}) {
+    this.setState({
+      selected: value
+    })
+  }
+
+  renderAnimates () {
+    const out = []
+    this.state.animates.forEach(animate => {
+      out.push(
+        <Form.Field key={generateID()}>
+          <Checkbox radio
+            label={animate.name}
+            name="animate"
+            value={animate.id}
+            checked={this.state.selected === animate.id}
+            onChange={this.handleSelect}
+          />
+        </Form.Field>
+      )
+    })
+    return out
+  }
+
+  renderAnimateSelect () {
+    if (!this.state.animates) {
+      return null
+    }
+    return <Form>{ this.renderAnimates() }</Form>
+  }
+
+  render () {
+    return <Fragment>
+      <Grid.Column style={{ marginTop: '2em', width: '85%' }}>
+        <h2>Update selected</h2>
+        { this.renderAnimateSelect() }
+        <br/>
+        {this.state.selected && <Button>Update selected Animate</Button>}
+
+        <Divider/>
+
+        <h2>Or add new</h2>
+        <Button onClick={this.handleAdd}>Add new Animate</Button>
+      </Grid.Column>
+    </Fragment>
+  }
+}
+
+export default connect(
+  state => ({
+    animates: configGetAllAnimates(state)
+  }),
+  dispatch => bindActionCreators({ addAnimate }, dispatch)
+)(AddAnimate)
---
+++ src/scenes/Add/AddAnimate/Uploader.jsx
@@ -0,0 +1,82 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { toast } from 'react-toastify'
+import { Redirect } from 'react-router-dom'
+import { Grid } from 'semantic-ui-react'
+
+import DropZone from '@components/DropZone'
+import BusySignal from '@components/BusySignal'
+
+import { addAnimate } from '@actions'
+
+import Runtime from '@runtime/templates/AnimateRuntime'
+import preprocess from './preprocess'
+import generateHash from '@helpers/generateHash'
+
+class Uploader extends Component {
+  constructor (props) {
+    super(props)
+    this.fileUploaded = this.fileUploaded.bind(this)
+    this.fileRejected = this.fileRejected.bind(this)
+    this.state = { pending: false }
+  }
+
+  fileRejected (files) {
+    const msg = `File '${files[0].name}' does not appear to be a .js`
+    toast.error(msg)
+  }
+
+  fileUploaded (files) {
+    const file = files[0]
+
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      preprocess(reader.result).then(preprocessed => {
+        const rootComponent = file.name.replace(/\.[^/.]+$/, '')
+        const runnable = Runtime.functionalizeSource(preprocessed)
+        const js = preprocessed
+
+        Runtime.getComponentNames(runnable, rootComponent).then(components => {
+          generateHash(js).then(hash => {
+            this.setState({ pending: false })
+            this.props.onUpload(js, hash, rootComponent, components)
+          })
+        }).catch(error => {
+          if (error instanceof TypeError) {
+            toast.error(`Could not load submitted js file, error: '${error.message}'.`)
+            toast.info('.js file should be exported by Adobe Animate CC (2017, 2018)')
+          } else if (error instanceof Error) {
+            const msg = `Error while initializing animate component: ${error.message}.`
+            toast.error(msg)
+            toast.info('Name of the js file must correspond to the name of the topmost component')
+          } else {
+            toast.error(error.message)
+          }
+          this.setState({ pending: false })
+        })
+      })
+    }
+    this.setState({pending: true})
+    reader.readAsText(file)
+  }
+
+  render () {
+    return <Fragment>
+      <Grid.Column style={{ marginTop: '2em', width: '85%' }}>
+        <BusySignal busy={this.state.pending} />
+        <DropZone display={true}
+          onDropAccepted={this.fileUploaded}
+          onDropRejected={this.fileRejected}
+          description='HTML5 Canvas .js from Animate CC'
+          accept='application/javascript, text/javascript'
+          imgSrc={`${process.env.PATH}/images/an.png`}
+        />
+      </Grid.Column>
+    </Fragment>
+  }
+}
+
+export default connect(null,
+  dispatch => bindActionCreators({ addAnimate }, dispatch)
+)(Uploader)
