93c8f283359b531e88f72a6df7d2dc5e633bf9c3
David Polak <git@davidpolak.cz>
2018-09-24 12:00:03 +0200

    Fix undefined reference in AnimatePlay

--- src/runtime/templates/widget/AnimatePlay/init.js
+++ src/runtime/templates/widget/AnimatePlay/init.js
@@ -1,6 +1,9 @@
 export default function initAnimatePlays () {
   return new Promise(resolve => {
     Object.entries(animates).forEach(([id, animate]) => {
+      if (animate.components === undefined) {
+        return
+      }
       animate.components.play.forEach(play => {
         play.onTick = (evtObj) => {
           play._tick(evtObj)
