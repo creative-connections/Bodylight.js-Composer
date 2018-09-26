df07e93d7862e7e7e647dab0eecdbc4c44c23c19
David Polak <git@davidpolak.cz>
2018-09-26 18:58:27 +0200

    Disable component placed indicator

--- src/scenes/Menu/components/Tree/components/TreeNode.jsx
+++ src/scenes/Menu/components/Tree/components/TreeNode.jsx
@@ -69,10 +69,6 @@ class TreeNode extends Component {
       <Icon name={this.getIcon()}/>
       {this.props.name}
 
-      { this.props.placed === true &&
-        <span title='Widget is placed in the editor, and will be exported' className='placed'>●</span>
-      }
-
       { this.state.collapsed === false &&
         <ul>{this.props.children}</ul>
       }
