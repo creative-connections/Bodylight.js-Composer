148b18d0ac3df3cca95ca4af69dfcac4f54b1527
David Polak <git@davidpolak.cz>
2018-11-01 16:36:00 +0100

    Update updateValueListeners oneshot for Array support

--- src/runtime/templates/model/oneshot/updateValueListeners.js
+++ src/runtime/templates/model/oneshot/updateValueListeners.js
@@ -1,9 +1,22 @@
 export default function updateValueListeners () {
   this.valueListeners.forEach(listener => {
-    listener.target.setValues(
-      listener.attribute,
-      this.batch.values[listener.index],
-      this.batch.time
-    )
+    if (listener.index) {
+      listener.target.setValues(
+        listener.attribute,
+        this.batch.values[listener.index],
+        this.batch.time
+      )
+    } else if (listener.indicies) {
+      const values = []
+      for (i = 0; i < this.batch.time.length; i++) {
+        values[i] = listener.indicies.map(index => this.batch.values[index][i])
+      }
+
+      listener.target.setArrays(
+        listener.attribute,
+        values,
+        this.batch.time
+      )
+    }
   })
 }
