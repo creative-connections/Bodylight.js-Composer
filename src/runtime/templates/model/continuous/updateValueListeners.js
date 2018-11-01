b0ca9f63014a6aa124638d03d02ad83434db0071
David Polak <git@davidpolak.cz>
2018-11-01 15:34:23 +0100

    Update updateValueListeners for array support

--- src/runtime/templates/model/continuous/updateValueListeners.js
+++ src/runtime/templates/model/continuous/updateValueListeners.js
@@ -1,9 +1,17 @@
 export default function updateValueListeners () {
   this.valueListeners.forEach(listener => {
-    listener.target.setValue(
-      listener.attribute,
-      this.outputValues[listener.index],
-      this.currentStep
-    )
+    if (listener.index) {
+      listener.target.setValue(
+        listener.attribute,
+        this.outputValues[listener.index],
+        this.currentStep
+      )
+    } else if (listener.indicies) {
+      listener.target.setArray(
+        listener.attribute,
+        listener.indicies.map(index => this.outputValues[index]),
+        this.currentStep
+      )
+    }
   })
 }
