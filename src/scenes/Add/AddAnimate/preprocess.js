b5649bd4e0921d30bc3c2fd0c201a8b79cc47697
David Polak <git@davidpolak.cz>
2018-11-07 18:12:02 +0100

    Fix preprocess for Animate CC 2019

--- src/scenes/Add/AddAnimate/preprocess.js
+++ src/scenes/Add/AddAnimate/preprocess.js
@@ -2,7 +2,7 @@ const preprocess = (file) => {
   return new Promise((resolve, reject) => {
   // TODO: this will need to be versioned for different animate versions
 
-  // Adobe Animate CC 2018
+    // Adobe Animate CC 2018
 
     const lines = file.match(/^.*((\r\n|\n|\r)|$)/gm)
     const outlines = []
@@ -27,12 +27,20 @@ const preprocess = (file) => {
 
       // matches lines like 'this.component.name = "component_anim"' retrieves
       // "this.component" and inserts it as a library exported component
-      const match = line.match('\\s(.*)\\.name\\s*=\\s*".*"')
+      let match = line.match('\\s(.*)\\.name\\s*=\\s*".*"')
       if (match) {
         const reference = match[1]
         const newline = `lib.addExportedComponent(${reference});`
         outlines.push(line)
         outlines.push(newline)
+        return
+      }
+
+      // global reference to AdobeAn in 'AdobeAn.Layer = new function() {'
+      // needs to be replaced with 'an'
+      match = line.match(/AdobeAn\.Layer\s*=\s*new\s*function\(\)\s*{/)
+      if (match) {
+        outlines.push('an.Layer = new function () {')
         return
       }
 
