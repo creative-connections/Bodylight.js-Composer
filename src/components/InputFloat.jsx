e3c6049e51aeecbc14b6c99a465079a531da29a0
David Polak <git@davidpolak.cz>
2018-12-03 21:22:48 +0100

    Add eE to InputFloat RegExp validation

--- src/components/InputFloat.jsx
+++ src/components/InputFloat.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Input } from 'semantic-ui-react'
 
-const floatRegExp = new RegExp('^[+-]?([0-9]+([.][0-9]*)?|[.][0-9]+)$')
+const floatRegExp = /^([+-])?([0-9]+([.][0-9]*)?|[.][0-9]+)?([eE]?)([-+]?)([0-9]*)$/
 
 const handleValidationOnChange = (e, v, onChange) => {
   const { value } = v
-  if (value === '' || value === '-' || floatRegExp.test(value)) {
+  if (value === '' || floatRegExp.test(value)) {
     onChange(e, v)
   }
 }
