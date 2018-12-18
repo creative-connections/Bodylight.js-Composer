382f1d319fd71f5d2d073c23e63fe87a8cd56291
David Polak <git@davidpolak.cz>
2018-12-18 15:12:24 +0100

    Add id to default config for Charts

--- src/reducers/config/reducers/charts/config/plotly/line/merge.js
+++ src/reducers/config/reducers/charts/config/plotly/line/merge.js
@@ -2,9 +2,11 @@ import update from 'immutability-helper'
 import defaultConfig from './default.js'
 
 export default (config, clear = false) => {
-
   // On library switch (clear == true) only keep the name from the previous configuration.
   if (clear) {
-    return update(defaultConfig, { name: { $set: config.name } })
+    return update(defaultConfig, {
+      id: { $set: config.id },
+      name: { $set: config.name }
+    })
   }
 }
