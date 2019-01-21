bb24119174ae5bf0d12cc33a580db90cda2b998a
David Polak <git@davidpolak.cz>
2019-01-21 12:29:37 +0100

    Add overflow-x and overflow-y

--- src/scenes/Design/Design.jsx
+++ src/scenes/Design/Design.jsx
@@ -64,13 +64,74 @@ class Design extends Component {
         'gjs-blocks-basic': {
           blocks: ['text', 'link', 'image', 'video', 'map']
         }
-      }
+      },
+
+      styleManager : {
+        sectors: [{
+          name: 'General',
+          open: false,
+          buildProps: ['float', 'display', 'position', 'top', 'right', 'left', 'bottom']
+        },{
+          name: 'Flex',
+          open: false,
+          buildProps: [
+            'flex-direction',
+            'flex-wrap',
+            'justify-content',
+            'align-items',
+            'align-content',
+            'order',
+            'flex-basis',
+            'flex-grow',
+            'flex-shrink',
+            'align-self'
+          ]
+        },{
+          name: 'Dimension',
+          open: false,
+          buildProps: [ 'width', 'height', 'max-width', 'min-height', 'margin', 'padding'],
+        },{
+          name: 'Typography',
+          open: false,
+          buildProps: [
+            'font-family',
+            'font-size',
+            'font-weight',
+            'letter-spacing',
+            'color',
+            'line-height',
+            'text-shadow'
+          ],
+        },{
+          name: 'Decorations',
+          open: false,
+          buildProps: [
+            'border-radius-c',
+            'background-color',
+            'border-radius',
+            'border',
+            'box-shadow',
+            'background'
+          ],
+        },{
+          name: 'Extra',
+          open: false,
+          buildProps: [
+            'transition',
+            'perspective',
+            'transform',
+            'overflow-x',
+            'overflow-y',
+            'overflow'
+          ],
+        }],
+      },
     })
 
     const cmdCanvasClear = 'canvas-clear'
     editor.Commands.add(cmdCanvasClear,
       e => confirm('This will clear the entire design, continue?') &&
-      e.runCommand('core:canvas-clear'))
+    e.runCommand('core:canvas-clear'))
     editor.Panels.addButton('options', {
       id: cmdCanvasClear,
       className: 'fa fa-trash',
