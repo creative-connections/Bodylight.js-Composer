19734bf5debd691ba3067c14bb71dee12fb9ca25
David Polak <git@davidpolak.cz>
2019-03-27 20:41:50 +0100

    Remove routes from DisplayContainer

--- src/scenes/DisplayContainer/DisplayContainer.jsx
+++ src/scenes/DisplayContainer/DisplayContainer.jsx
@@ -1,30 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
 import { getApplicationKey, getPreview } from '@reducers'
 
 import Preview from '@scenes/Preview'
 import Design from '@scenes/Design'
-import AddModel from '@scenes/Add/AddModel'
 
 class DisplayContainer extends Component {
-  constructor (props) {
-    super(props)
-    this.getDesignKey = this.getDesignKey.bind(this)
-  }
-
-  getDesignKey () {
-    return <Design key={this.props.appkey}/>
-  }
-
   render () {
-    return <Fragment>
-      <div id='DisplayContainer'>
-        {this.props.preview === false && this.getDesignKey()}
-        {this.props.preview && <Preview/>}
-        <Route path={`${process.env.PATH}/add/model`} component={AddModel} />
-      </div>
-    </Fragment>
+    return <div id='DisplayContainer'>
+      {this.props.preview === false && <Design key={this.props.appkey}/>}
+      {this.props.preview && <Preview/>}
+    </div>
   }
 }
 
