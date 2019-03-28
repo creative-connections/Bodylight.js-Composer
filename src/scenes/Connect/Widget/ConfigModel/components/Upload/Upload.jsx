85197fe1976fc99b1c807dabd7cd7b90ec49c638
David Polak <git@davidpolak.cz>
2019-03-28 18:51:36 +0100

    Add first page to tutorial

---
+++ src/scenes/Connect/Widget/ConfigModel/components/Upload/Upload.jsx
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { populateModel } from '@actions'
+import GridRow from '@components/GridRow'
+
+class Upload extends Component {
+  constructor(props) {
+    super(props)
+    this.defaultState = {
+      uploader: true,
+      updater: false,
+      uploaded: null
+    }
+
+    this.state = this.defaultState
+    this.handleUpload = this.handleUpload.bind(this)
+    this.handleUpdate = this.handleUpdate.bind(this)
+  }
+
+  handleUpdate(success = false) {
+    if (!success) {
+      this.setState(this.defaultState)
+    }
+  }
+
+  handleUpload() {
+  }
+
+  render() {
+    return <Fragment>
+      <GridRow label='Upload model'>
+      </GridRow>
+    </Fragment>
+  }
+}
+
+export default connect(null, dispatch => bindActionCreators({
+  populateModel
+}, dispatch))(Upload)
