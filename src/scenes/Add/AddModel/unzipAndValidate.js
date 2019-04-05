5201c6f68c200147b3cf3c301d0962ba49aa7201
David Polak <git@davidpolak.cz>
2019-04-05 16:03:32 +0200

    Remove name requirement for model file
    No longer needs to match the .js filename

--- src/scenes/Add/AddModel/unzipAndValidate.js
+++ src/scenes/Add/AddModel/unzipAndValidate.js
@@ -2,13 +2,24 @@ import Zip from 'jszip'
 import toAST from 'to-ast'
 import escodegen from 'escodegen'
 
-const validateContains = (zip, name) => {
-  if (zip.files[name + '.js'] === undefined) {
-    throw Error(`Zip file does not contain the compiled JavaScript file ''${name}.js'`)
-  }
+const validateContains = (zip) => {
   if (zip.files['modelDescription.xml'] === undefined) {
     throw Error("Zip file does not contain the 'modelDescription.xml' file ")
   }
+}
+
+const locateJs = (zip) => {
+  let file = null
+  Object.entries(zip.files).forEach(([key, f]) => {
+    if (key.endsWith('js')) {
+      file = f
+    }
+  })
+
+  if (!file) {
+    throw Error("Zip file does not contain the 'modelDescription.xml' file ")
+  }
+  return file
 }
 
 const postProcessJs = (js, name) => {
@@ -25,21 +36,21 @@ export default function unzipAndValidate (file) {
   return new Promise((resolve, reject) => {
     const jszip = new Zip()
 
-    const name = file.name.replace(/\.[^/.]+$/, '')
-
     jszip.loadAsync(file).then((zip) => {
       try {
-        validateContains(zip, name)
+        validateContains(zip)
       } catch (error) {
         return reject(error)
       }
 
-      const js = zip.files[name + '.js'].async('string')
+      const jsfile = locateJs(zip)
+      const js = jsfile.async('string')
+      const name = jsfile.name.slice(0,-3)
       const xml = zip.files['modelDescription.xml'].async('string')
 
       Promise.all([js, xml]).then((vals) => {
         const payload = {
-          name: name,
+          name,
           js: postProcessJs(vals[0], name),
           modelDescription: vals[1]
         }
