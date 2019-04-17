d14c7ce6aaec49c87677270f381170ffb7d49925
David Polak <git@davidpolak.cz>
2019-04-17 16:26:17 +0200

    Refactor ConfigModel Upload

--- src/scenes/Connect/Widget/ConfigModel/components/Upload/Upload.jsx
+++ src/scenes/Connect/Widget/ConfigModel/components/Upload/Upload.jsx
@@ -1,27 +1,72 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import { toast } from 'react-toastify'
 
-import { populateModel } from '@actions'
+import DropZone from '@components/DropZone'
+import BusySignal from '@components/BusySignal'
+import unzipAndValidate from './unzipAndValidate'
+import parseModelDescription from './parseModelDescription'
+import generateHash from '@helpers/generateHash'
 import GridRow from '@components/GridRow'
 
-import Uploader from './Uploader'
+import { populateModel, updateModel } from '@actions'
 
-class Upload extends Component {
+class Uploader extends Component {
   constructor(props) {
     super(props)
-    this.state = this.defaultState
+    this.fileUploaded = this.fileUploaded.bind(this)
+    this.fileRejected = this.fileRejected.bind(this)
+    this.state = { pending: false }
+  }
+
+  fileRejected(files) {
+    const msg = `File '${files[0].name}' does not appear to be a .zip`
+    toast.error(msg)
+  }
+
+  fileUploaded(files) {
+    const file = files[0]
+    this.setState({
+      pending: true
+    })
+    unzipAndValidate(file).then((files) => {
+      let name = files['name']
+      let js = files['js']
+      let modelDescription = files['modelDescription']
+      modelDescription = parseModelDescription(modelDescription)
+      generateHash(js).then(hash => {
+        this.setState({ pending: false })
+        if (this.props.model.populated === true) {
+          this.props.updateModel(this.props.model.id, name, js, hash, modelDescription)
+        }
+        else {
+          this.props.populateModel(this.props.model.id, name, js, hash, modelDescription)
+        }
+        this.props.onUpdate()
+      })
+    }).catch((err) => {
+      const msg = `Error while extracting zip file: ${err.message}`
+      toast.error(msg)
+      this.setState({ pending: false })
+      throw err
+    })
   }
 
   render() {
-    return <Fragment>
-      <GridRow label='Upload model'>
-        <Uploader model={this.props.model} onUpdate={this.props.onUpdate}/>
-      </GridRow>
-    </Fragment>
+    return <GridRow label='Upload model'>
+      <BusySignal busy={this.state.pending} />
+      <DropZone display={true}
+        className='dropzone'
+        onDropAccepted={this.fileUploaded}
+        onDropRejected={this.fileRejected}
+        description='.zip file from the Bodylight.js compiler'
+        accept='application/zip, application/x-zip, application/x-zip-compressed, multipart/x-zip, application/zip-compressed'
+      />
+    </GridRow>
   }
 }
 
-export default connect(null, dispatch => bindActionCreators({
-  populateModel
-}, dispatch))(Upload)
+export default connect(null,
+  dispatch => bindActionCreators({ populateModel, updateModel }, dispatch)
+)(Uploader)
